c52934ba3cd11234eee2cc49835340c3b35004c5
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-11-22 23:35:38 +0300

    new full version of project
    I didn't want to lose a few of my commits when my project crashed, so I reload all files. This commit represents fully working project, if you want to test it, download this commit

---
+++ Task4_3/AppCore/Antenna.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task4_3.AppCore
+{
+    public class Antenna
+    {
+        private String _name;
+        private Int32 _power;
+        private Double _height;
+
+        public Antenna(string name, int power, double height)
+        {
+            _name = name;
+            _power = power;
+            _height = height;
+        }
+
+        public virtual Double Quality()
+        {
+            return _power + 0.5 * _height;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int Power
+        {
+            get => _power;
+            set => _power = value;
+        }
+
+        public double Height
+        {
+            get => _height;
+            set => _height = value;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + _name +
+                   " Power: " + _power +
+                   " Height: " + _height +
+                   " Quality: " + Quality();
+        }
+    }
+}
---
+++ Task4_3/AppCore/BetterAntenna.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task4_3.AppCore
+{
+    public class BetterAntenna : Antenna
+    {
+        private Double _emissivity;
+
+        public BetterAntenna(string name, int power, double height, double emissivity) : base(name, power, height)
+        {
+            _emissivity = emissivity;
+        }
+
+        public override Double Quality()
+        {
+            return base.Quality() - 0.1 * _emissivity;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " Emissivity: " + _emissivity;
+        }
+    }
+}
